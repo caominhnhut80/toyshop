999f5f6b5c5b731365738d081e9957cc4cfb779a
caominhnhut80 <32754575+caominhnhut80@users.noreply.github.com>
2017-10-13 09:31:11 +0700

    12445666

--- toyshop/thong_new.Designer.cs
+++ toyshop/thong_new.Designer.cs
@@ -33,19 +33,22 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(414, 179);
+            this.button1.Location = new System.Drawing.Point(310, 145);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(56, 19);
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // thong_new
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(924, 480);
+            this.BackColor = System.Drawing.Color.Navy;
+            this.ClientSize = new System.Drawing.Size(693, 390);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "thong_new";
             this.Text = "thong_new";
             this.ResumeLayout(false);
