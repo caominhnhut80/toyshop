a098b6266b3de8fd22f6ba348a83396bbdae4c36
caominhnhut80 <32754575+caominhnhut80@users.noreply.github.com>
2017-10-16 12:26:50 +0700

    fhhh

---
+++ toyshop/report/rp_bc_all_phieunhap.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace toyshop.report
+{
+    public partial class rp_bc_all_phieunhap : Form
+    {
+        public rp_bc_all_phieunhap()
+        {
+            InitializeComponent();
+        }
+    }
+}
